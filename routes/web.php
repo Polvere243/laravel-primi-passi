35465ac7ba98197274009da835525e84e8ffaecc
Polvere243 <danielercoli@gmail.com>
2024-03-07 15:10:47 +0100

    hardcoded a string

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Hello World';
+    $subject = 'by Classe 116';
+    return view('home', compact('subject'));
 });
